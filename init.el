9a58842ed7b273532b7334b4edc80958684dcfdd
mneko22 <c01144737e@edu.teu.ac.jp>
2017-06-06 10:39:37 +0900

    余計なところの削除

--- init.el
+++ init.el
@@ -12,87 +12,15 @@
 (setq inhibit-startup-screen t)
 (global-linum-mode t)
 
+;; shortcut settings
+(keyboard-translate ?\C-h ?\C-?)
+
 ;; cask initalize
 (cond ((equal system-type 'darwin)
        (require 'cask "/usr/local/Cellar/cask/0.8.1/cask.el"))
       ((equal system-type 'gnu/linux)
        (require 'cask "~/.cask/cask.el")))
 (cask-initialize)
-
-;; thme settings
-(load-theme 'monokai t)
-;; power line
-(require 'powerline)
-(powerline-default-theme)
-
-
-;; input support config
-;; autocomplete
-(require 'auto-complete)
-(ac-config-default)
-
-;; flycheck
-(require 'flycheck)
-(global-flycheck-mode)
-
-;; yasnippet
-(require 'yasnippet)
-(setq yas-snippet-dirs
-      '("~/.emacs.d/plugins/snippets/personal/" ;; personal snippets
-        "~/.emacs.d/plugins/snippets/yasnippet-snippets";; the yasmate collection
-        ))
-(yas-global-mode 1)
-
-;; helm
-(require 'helm-config)
-(helm-mode 1)
-
-;; git config
-(require 'magit)
-
-;; web-mode
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-;; smarty-mode
-(require 'smarty-mode)
-(add-to-list 'auto-mode-alist '("\\.smarty\\'" . smarty-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\'" . smarty-mode))
-
-
-;; markdown-modepp
-(require 'markdown-mode)
-(use-package markdown-mode
-  :ensure t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-	 ("\\.md\\'" . markdown-mode)
-	 ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
-
-
-;; go-mode
-(add-to-list 'exec-path (expand-file-name "/opt/go/bin/"))
-(add-to-list 'exec-path (expand-file-name "~/.go/bin/"))
-(require 'go-mode)
-(require 'go-autocomplete)
-(add-hook 'go-mode-hook 'flycheck-mode)
-
-;; python mode config
-(require 'python)
-(defun python-shell-parse-command ()
-  "Return the string used to execute the inferior Python process."
-  "python3 -i"
-  )
-
-(keyboard-translate ?\C-h ?\C-?)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -107,4 +35,69 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; thme settings
+(load-theme 'monokai t)
+;; power line
+(powerline-default-theme)
+
+
+;; input support config
+;; autocomplete
+(ac-config-default)
+
+;; flycheck
+(global-flycheck-mode)
+
+;; yasnippet
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/plugins/snippets/personal/" ;; personal snippets
+        "~/.emacs.d/plugins/snippets/yasnippet-snippets";; the yasmate collection
+        ))
+(yas-global-mode 1)
+
+;; helm
+(helm-mode 1)
+
+;; git config
+
+
+;; web-mode
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; smarty-mode
+(add-to-list 'auto-mode-alist '("\\.smarty\\'" . smarty-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . smarty-mode))
+
+
+;; markdown-modepp
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+
+;; go-mode
+(add-to-list 'exec-path (expand-file-name "/opt/go/bin/"))
+(add-to-list 'exec-path (expand-file-name "~/.go/bin/"))
+(add-hook 'go-mode-hook 'flycheck-mode)
+
+;; python mode config
+(require 'python)
+(defun python-shell-parse-command ()
+  "Return the string used to execute the inferior Python process."
+  "python3 -i"
+  )
+
 ;;; init.el ends here

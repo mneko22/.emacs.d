42340ffabacceefacdbc0b15ac4f2fa70635f3f7
mneko22 <c01144737e@edu.teu.ac.jp>
2023-01-22 11:15:48 +0900

    change package manager

--- init.el
+++ init.el
@@ -1,59 +1,130 @@
-;; global settings
-(setq inhibit-startup-screen t)
-(menu-bar-mode -1)
-(tool-bar-mode 0)
-(global-linum-mode t)
-(prefer-coding-system 'utf-8)
-(set-default 'buffer-file-coding-system 'utf-8)
+;; initialize package manger
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
 
-;; global shortcut settings
-(keyboard-translate ?\C-h ?\C-?)
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("org"   . "https://orgmode.org/elpa/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
 
-;; initialize el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
 
 
-(el-get-bundle which-key
-  (which-key-mode)
-  (which-key-setup-side-window-right)
-  (defvar which-key-side-window-max-width 0.20))
-(el-get-bundle magit)
-(el-get-bundle trr)
-(el-get-bundle company
-  (add-hook 'after-init-hook 'global-company-mode))
-(el-get-bundle lsp-mode
-  (add-hook 'go-mode-hook #'lsp-deferred))
-(el-get-bundle projectile
-  (projectile-mode +1)
-  (defvar projectile-completion-system 'helm))
-(el-get-bundle markdown-mode)
-(el-get-bundle editorconfig)
-(el-get-bundle org-mode)
-(el-get-bundle smartparens
-  (smartparens-global-mode))
-(el-get-bundle doom-themes
-  (defvar doom-themes-enable-bold t)    ; if nil, bold is universally disabled
-  (defvar doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t))
-(el-get-bundle doom-modeline
-  (add-hook 'after-init-hook #'doom-modeline-mode)
-  (defvar doom-modeline-height 1)
-  (defvar doom-modeline-icon nil))
-(el-get-bundle go-mode)
-(el-get-bundle helm
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  (defvar helm-M-x-fuzzy-match t)
-  (global-set-key (kbd "C-x b") 'helm-mini)
-  (defvar helm-buffers-fuzzy-matching t)
-  (defvar helm-recentf-fuzzy-match t)
-  (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  (helm-mode 1))
+;; editor settings
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :tag "builtin" "faces" "help"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
 
-(el-get 'sync)
+
+
+(leaf cus-start
+  :doc "define customization properties of builtins"
+  :tag "builtin" "internal"
+  :preface
+  (defun c/redraw-frame nil
+    (interactive)
+    (redraw-frame))
+
+  :bind (("M-ESC ESC" . c/redraw-frame))
+  :custom '((create-lockfiles . nil)
+            (debug-on-error . nil)
+            (init-file-debug . t)
+            (frame-resize-pixelwise . t)
+            (enable-recursive-minibuffers . t)
+            (history-length . 1000)
+            (history-delete-duplicates . t)
+            (scroll-preserve-screen-position . t)
+            (scroll-conservatively . 100)            
+            (ring-bell-function . 'ignore)
+            (text-quoting-style . 'straight)
+            (truncate-lines . t)
+            (menu-bar-mode . nil)
+            (tool-bar-mode . nil)
+            (scroll-bar-mode . nil)
+	    (global-display-line-numbers-mode . t)
+            (indent-tabs-mode . nil))
+  :config
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (keyboard-translate ?\C-h ?\C-?))
+
+
+
+(leaf files
+  :doc "file input and output commands for Emacs"
+  :tag "builtin"
+  :custom `((auto-save-timeout . 15)
+            (auto-save-interval . 60)
+            (auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
+            (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
+                                        (,tramp-file-name-regexp . nil)))
+            (version-control . t)
+            (delete-old-versions . t)))
+
+(leaf startup
+  :doc "process Emacs shell arguments"
+  :tag "builtin" "internal"
+  :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))))
+
+
+(leaf theme
+  :config
+  (leaf doom-themes
+    :ensure t
+    :config
+    (defvar doom-themes-enable-bold t)
+    (defvar doom-themes-enable-italic t)
+    (load-theme 'doom-one t))
+  (leaf doom-modeline
+    :ensure t
+    :config
+    (add-hook 'after-init-hook #'doom-modeline-mode)
+    (defvar doom-modeline-height 1)
+    (defvar doom-modeline-icon nil)))
+
+(leaf drag-stuff
+  :doc "drag stuff (words, region, lines) around"
+  :ensure t
+  :config
+  (drag-stuff-global-mode)
+  (drag-stuff-define-keys)
+  (setq drag-stuff-modifier '(meta shift)))
+
+
+(leaf magit
+  :doc "git client"
+  :ensure t
+)
+
+(leaf dashboard
+  :doc "custimize startup screen package"
+  :ensure t
+  :init
+  (dashboard-setup-startup-hook)
+  :config
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+  (setq dashboard-startup-banner 'logo))
+
+
+
+
+;;end of editor setting
+(provide 'init)
